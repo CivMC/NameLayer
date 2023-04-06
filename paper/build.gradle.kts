0a8ab9f42051e6f0d33e85f5cdab453dfc126899
Cola <vaultslave@gmail.com>
2023-04-06 01:05:42 +0100

    v3.1.0 - 1.19.4

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -11,7 +11,7 @@ civGradle {
 }
 
 dependencies {
-	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+	paperDevBundle("1.19.4-R0.1-SNAPSHOT")
 
-	compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
+	compileOnly("net.civmc.civmodcore:civmodcore-paper:2.3.5:dev-all")
 }
